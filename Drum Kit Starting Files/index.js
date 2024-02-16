44694c8fa43f035ff64686b95b133e65c7accde0
utkarsh215 <115106879+utkarsh215@users.noreply.github.com>
2024-02-16 16:36:55 +0530

    Update index.js
    added a button animation

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,41 +4,25 @@ for(var i=0;i<7;i++)
 {
     document.querySelectorAll(".drum")[i].addEventListener("click",function ()  
     {
-        console.log(this.innerHTML);
-        switch(this.innerHTML)
-        {
-            case 'w':
-                var audio=new Audio("sounds/tom-1.mp3");
-            break;
-            case 'a':
-                 audio=new Audio("sounds/tom-2.mp3");
-            break;
-            case 's':
-                 audio=new Audio("sounds/tom-3.mp3");
-            break;
-            case 'd':
-                 audio=new Audio("sounds/tom-4.mp3");
-            break;
-            case 'j':
-                 audio=new Audio("sounds/snare.mp3");
-            break;
-            case 'k':
-                 audio=new Audio("sounds/kick-bass.mp3");
-            break;
-            case 'l':
-                 audio=new Audio("sounds/crash.mp3");
-            break;
-        }
-        audio.play();
+        playAudio(this.innerHTML);
+        buttonAnimation(this.innerHTML);
         
     });
 
 }
 document.addEventListener("keydown" , function (event){
-     switch(event.key)
+     playAudio(event.key);
+     buttonAnimation(event.key);
+});
+
+
+
+function playAudio(key)
+{
+     switch(key)
      {
           case 'w':
-                var audio=new Audio("sounds/tom-1.mp3");
+                 audio=new Audio("sounds/tom-1.mp3");
             break;
             case 'a':
                  audio=new Audio("sounds/tom-2.mp3");
@@ -60,4 +44,14 @@ document.addEventListener("keydown" , function (event){
             break;
      }
      audio.play();
-})
+}
+
+
+function buttonAnimation(currentButton){
+
+     document.querySelector("."+currentButton).classList.add("pressed");
+     
+     setTimeout(function (){
+          document.querySelector("."+currentButton).classList.remove("pressed");
+     } , 100);
+}
